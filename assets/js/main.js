4e0af8a831666b70d6b9ef69dd7eb88b4d12be20
Tien <tn6609092@gmail.com>
2022-11-25 22:37:20 +0700

    sửa lại phần của Gia Bảo: sửa lại giao diện header, foote; xử lý js đăng ký, đăng nhập

--- assets/js/main.js
+++ assets/js/main.js
@@ -5,52 +5,53 @@ function giay(productId,brand,img,name,size,price,quantity) {
     this.brand = brand;
     this.img = img;
     this.name = name;
-	this.size = size;
     this.price = price;
 	this.quantity = quantity;
 }
 
 var Adidas = [
-	new giay("A01-36","Adidas","/Adidas/Adidas Ultraboost FZ2558 Men.png","Adidas Ultraboost FZ2558 Men","36",5200000,10),
-	new giay("A01-37","Adidas","/Adidas/Adidas Ultraboost FZ2558 Men.png","Adidas Ultraboost FZ2558 Men","37",5200000,10),
-	new giay("A01-38","Adidas","/Adidas/Adidas Ultraboost FZ2558 Men.png","Adidas Ultraboost FZ2558 Men","38",5200000,10),
-	new giay("A02-40","Adidas","/Adidas/Adidas Ultraboost GX5928 Women.png","Adidas Ultraboost GX5928 Women","40",5000000,10),
-	new giay("A02-42","Adidas","/Adidas/Adidas Ultraboost GX5928 Women.png","Adidas Ultraboost GX5928 Women","42",5000000,10),
-	new giay("A03-39","Adidas","/Adidas/Unisex Adidas Ultraboost GY4173.jpg","Unisex Adidas Ultraboost GY4173","39",5000000,10),
+	new giay("A01","Adidas","/Adidas/Adidas Ultraboost FZ2558 Men.png","Adidas Ultraboost FZ2558 Men",5200000,10),
+	new giay("A01","Adidas","/Adidas/Adidas Ultraboost FZ2558 Men.png","Adidas Ultraboost FZ2558 Men",5200000,10),
+	new giay("A01","Adidas","/Adidas/Adidas Ultraboost FZ2558 Men.png","Adidas Ultraboost FZ2558 Men",5200000,10),
+	new giay("A02","Adidas","/Adidas/Adidas Ultraboost GX5928 Women.png","Adidas Ultraboost GX5928 Women",5000000,10),
+	new giay("A02","Adidas","/Adidas/Adidas Ultraboost GX5928 Women.png","Adidas Ultraboost GX5928 Women",5000000,10),
+	new giay("A03","Adidas","/Adidas/Unisex Adidas Ultraboost GY4173.jpg","Unisex Adidas Ultraboost GY4173",5000000,10),
 ];
 
 var Nike = [
-	new giay("N01-36","Nike","/Nike/Nike Air Max DC9332-001 Men.jpg","Nike Air Max DC9332-001 Men","36",5000000,10),
-	new giay("N01-37","Nike","/Nike/Nike Air Max DC9332-001 Men.jpg","Nike Air Max DC9332-001 Men","37",5000000,10),
-	new giay("N01-38","Nike","/Nike/Nike Air Max DC9332-001 Men.jpg","Nike Air Max DC9332-001 Men","38",5000000,10),
-	new giay("N02-40","Nike","/Nike/Nike Air Max DH4245-400 Men.jpg","Nike Air Max DH4245-400 Men","40",4700000,10),
-	new giay("N02-42","Nike","/Nike/Nike Air Max DH4245-400 Men.jpg","Nike Air Max DH4245-400 Men","42",4700000,10),
-	new giay("N03-39","Nike","/Nike/Unisex Nike CV7562-104.jpg","Unisex Nike CV7562-104","39",4700000,10),
+	new giay("N01","Nike","/Nike/Nike Air Max DC9332-001 Men.jpg","Nike Air Max DC9332-001 Men",5000000,10),
+	new giay("N01","Nike","/Nike/Nike Air Max DC9332-001 Men.jpg","Nike Air Max DC9332-001 Men",5000000,10),
+	new giay("N01","Nike","/Nike/Nike Air Max DC9332-001 Men.jpg","Nike Air Max DC9332-001 Men",5000000,10),
+	new giay("N02","Nike","/Nike/Nike Air Max DH4245-400 Men.jpg","Nike Air Max DH4245-400 Men",4700000,10),
+	new giay("N02","Nike","/Nike/Nike Air Max DH4245-400 Men.jpg","Nike Air Max DH4245-400 Men",4700000,10),
+	new giay("N03","Nike","/Nike/Unisex Nike CV7562-104.jpg","Unisex Nike CV7562-104",4700000,10),
 ];
 
 var Puma = [
-	new giay("P01-36","Puma","/Puma/PUMA SERVE PRO 1948 TEAM GOLD Men.jpg","PUMA SERVE PRO 1948 TEAM GOLD Men","36",2700000,10),
-	new giay("P01-37","Puma","/Puma/PUMA SERVE PRO 1948 TEAM GOLD Men.jpg","PUMA SERVE PRO 1948 TEAM GOLD Men","37",2700000,10),
-	new giay("P01-38","Puma","/Puma/PUMA SERVE PRO 1948 TEAM GOLD Men.jpg","PUMA SERVE PRO 1948 TEAM GOLD Men","38",2700000,10),
-	new giay("P02-40","Puma","/Puma/PUMA RBD GAME LOW Men.jpg","PUMA RBD GAME LOW","40",2800000,10),
-	new giay("P02-42","Puma","/Puma/PUMA RBD GAME LOW Men.jpg","PUMA RBD GAME LOW","42",2800000,10),
-	new giay("P03-39","Puma","/Puma/PUMA SOFTRIDE ENZO NXT Men.jpg","PUMA SOFTRIDE ENZO NXT","39",2200000,10),
+	new giay("P01","Puma","/Puma/PUMA SERVE PRO 1948 TEAM GOLD Men.jpg","PUMA SERVE PRO 1948 TEAM GOLD Men",2700000,10),
+	new giay("P01","Puma","/Puma/PUMA SERVE PRO 1948 TEAM GOLD Men.jpg","PUMA SERVE PRO 1948 TEAM GOLD Men",2700000,10),
+	new giay("P01","Puma","/Puma/PUMA SERVE PRO 1948 TEAM GOLD Men.jpg","PUMA SERVE PRO 1948 TEAM GOLD Men",2700000,10),
+	new giay("P02","Puma","/Puma/PUMA RBD GAME LOW Men.jpg","PUMA RBD GAME LOW",2800000,10),
+	new giay("P02","Puma","/Puma/PUMA RBD GAME LOW Men.jpg","PUMA RBD GAME LOW",2800000,10),
+	new giay("P03","Puma","/Puma/PUMA SOFTRIDE ENZO NXT Men.jpg","PUMA SOFTRIDE ENZO NXT",2200000,10),
 ];
 
 var Converse = [
-	new giay("C01-36","Converse","/Converse/Converse Chuck 70 Seasonal Color - A01448C.jpg","Converse Chuck 70 Seasonal Color","36",1900000,10),
-	new giay("C01-37","Converse","/Converse/Converse Chuck 70 Seasonal Color - A01448C.jpg","Converse Chuck 70 Seasonal Color","37",1900000,10),
-	new giay("C01-38","Converse","/Converse/Converse Chuck 70 Seasonal Color - A01448C.jpg","Converse Chuck 70 Seasonal Color","38",1900000,10),
-	new giay("C02-40","Converse","/Converse/Converse Chuck Taylor All Star 70 Plus.jpg","Converse Chuck Taylor All Star 70 Plus","40",2500000,10),
-	new giay("C02-42","Converse","/Converse/Converse Chuck Taylor All Star 70 Plus.jpg","Converse Chuck Taylor All Star 70 Plus","42",2500000,10),
-	new giay("C03-39","Converse","/Converse/Converse Chuck Taylor All Star Dainty.jpg","Converse Chuck Taylor All Star Dainty","39",1500000,10),
+	new giay("C01","Converse","/Converse/Converse Chuck 70 Seasonal Color - A01448C.jpg","Converse Chuck 70 Seasonal Color",1900000,10),
+	new giay("C01","Converse","/Converse/Converse Chuck 70 Seasonal Color - A01448C.jpg","Converse Chuck 70 Seasonal Color",1900000,10),
+	new giay("C01","Converse","/Converse/Converse Chuck 70 Seasonal Color - A01448C.jpg","Converse Chuck 70 Seasonal Color",1900000,10),
+	new giay("C02","Converse","/Converse/Converse Chuck Taylor All Star 70 Plus.jpg","Converse Chuck Taylor All Star 70 Plus",2500000,10),
+	new giay("C02","Converse","/Converse/Converse Chuck Taylor All Star 70 Plus.jpg","Converse Chuck Taylor All Star 70 Plus",2500000,10),
+	new giay("C03","Converse","/Converse/Converse Chuck Taylor All Star Dainty.jpg","Converse Chuck Taylor All Star Dainty",1500000,10),
 ];
 
 var Vans = [
-	new giay("V01-36","Vans","/Vans/Vans Old Skool Checker.jpg","Vans Old Skool Checker","36",1600000,10),
-	new giay("V01-37","Vans","/Vans/Vans Old Skool Checker.jpg","Vans Old Skool Checker","37",1600000,10),
-	new giay("V01-38","Vans","/Vans/Vans Old Skool Checker.jpg","Vans Old Skool Checker","38",1600000,10),
-	new giay("V02-40","Vans","/Vans/Vans SK8-Hi BW.jpg","Vans SK8-Hi BW","40",1000000,10),
-	new giay("V02-42","Vans","/Vans/Vans SK8-Hi BW.jpg","Vans SK8-Hi BW","42",1000000,10),
-	new giay("V03-39","Vans","/Vans/Vans Old Skool Black White.jpg","Vans Old Skool Black White","39",1600000,10),
+	new giay("V01","Vans","/Vans/Vans Old Skool Checker.jpg","Vans Old Skool Checker",1600000,10),
+	new giay("V01","Vans","/Vans/Vans Old Skool Checker.jpg","Vans Old Skool Checker",1600000,10),
+	new giay("V01","Vans","/Vans/Vans Old Skool Checker.jpg","Vans Old Skool Checker",1600000,10),
+	new giay("V02","Vans","/Vans/Vans SK8-Hi BW.jpg","Vans SK8-Hi BW",1000000,10),
+	new giay("V02","Vans","/Vans/Vans SK8-Hi BW.jpg","Vans SK8-Hi BW",1000000,10),
+	new giay("V03","Vans","/Vans/Vans Old Skool Black White.jpg","Vans Old Skool Black White",1600000,10),
 ];
+
+
--- assets/js/user.js
+++ assets/js/user.js
@@ -1,3 +1,377 @@
+//Tài Khoản Admin---------------------------------------------------------------------------------------------------------------------------------------------------------------------
+var adminTk = "Admin";
+var adminMk = "1111";
+
+// -----------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
+({
+    plugin: ['jsdom-quokka-plugin'],
+    jsdom: 
+    {
+        file: "do_an.html"
+    }
+})
+//bỏ cái ở trên
+
+
+let slash = document.getElementsByClassName("slash");
+let password_input = document.getElementsByClassName("password");
+const see_hide = document.getElementsByClassName("place");
+const exits = document.getElementsByClassName("exit");
+let form_1 = document.getElementById("sign_up").style.zIndex, form_2 = document.getElementById("sign_in").style.zIndex; 
+
+
+function moDangKy() {
+    document.getElementById("sign_up").style.zIndex = 1;
+}
+
+function dongDangKy() {
+    document.getElementById("sign_up").style.zIndex = -1;      
+    for (let i = 0; i < password_input.length; i++) {
+        if ( password_input[i].type === "text")
+        {
+            password_input[i].type = "password";
+            slash[i].style.zIndex = 1;
+        }
+    }
+    var formElement = document.getElementById("sign_up");
+    var inputElement = formElement.querySelectorAll(".input_form");
+    for (let index = 0; index < inputElement.length; index++) {
+        inputElement[index].value = "";            
+    }
+}
+
+function moDangNhap() {
+    document.getElementById("sign_in").style.zIndex = 1;
+}
+
+function dongDangNhap() {
+    document.getElementById("sign_in").style.zIndex = -1;
+    var formElement = document.getElementById("sign_in");
+    var inputElement = formElement.querySelectorAll(".input_form");
+    for (let index = 0; index < inputElement.length; index++) {
+        inputElement[index].value = "";            
+    }
+}
+
+
+for (let i = 0; i < see_hide.length; i++)
+    see_hide[i].onclick = () =>
+    {
+        if ( password_input[i].type === "password")
+        {
+            password_input[i].type = "text";
+            slash[i].style.zIndex = -1;
+        }
+        else
+        {
+            password_input[i].type = "password";
+            slash[i].style.zIndex = 1;
+        }
+        password_input[i].focus();
+    }
+
+	
+
+    function dangKy() {
+        var formElement = document.getElementById("sign_up");
+        var inputElement = formElement.querySelectorAll(".input_form");
+        var matkhau = document.getElementById("password_1");
+        var nhaplaimk = document.getElementById("password_2");
+        // kiểm tra bỏ trống thông tin
+        for (let index = 0; index < inputElement.length; index++) {
+            if  (inputElement[index].value === "") {
+                if (inputElement[index].parentElement.className === "see_hide_pass") {
+                    inputElement[index].parentElement.parentElement.querySelector(".error-message").innerText = `Vui lòng nhập ${inputElement[index].name}`;
+                    inputElement[index].parentElement.parentElement.querySelector(".error-message").style.color = "red";
+                    inputElement[index].focus();
+                    return false;
+                }
+                else {
+                    inputElement[index].parentElement.querySelector(".error-message").innerText = `Vui lòng nhập ${inputElement[index].name}`;
+                    inputElement[index].parentElement.querySelector(".error-message").style.color = "red";
+                    inputElement[index].focus();
+                    return false;
+                }
+            }
+            else if (matkhau.value !== nhaplaimk.value) {
+                nhaplaimk.parentElement.parentElement.querySelector(".error-message").innerText = `Nhập đúng mật khẩu vừa đặt`;
+                nhaplaimk.parentElement.parentElement.querySelector(".error-message").style.color = "red";
+                matkhau.parentElement.parentElement.querySelector(".error-message").innerText = "";
+                nhaplaimk.focus();
+                return false;
+            }
+            else {
+                if (inputElement[index].parentElement.className === "see_hide_pass") {
+                    inputElement[index].parentElement.parentElement.querySelector(".error-message").innerText = "";
+                }
+                else {
+                    inputElement[index].parentElement.querySelector(".error-message").innerText = "";
+                }
+                
+            }
+        }
+
+        // xử lý đăng ký thành công ==> lưu tài khoản vào localStorage
+        var hoten = document.getElementById("hoten");
+        var email = document.getElementById("email");
+        var sdt = document.getElementById("sdt");
+
+        var listTaiKhoan = localStorage.getItem("listTaiKhoan") ? JSON.parse(localStorage.getItem("listTaiKhoan")) : [];
+        var listGioHang = localStorage.getItem("listGioHang") ? JSON.parse(localStorage.getItem("listGioHang")) : [];
+        var listDonHang = localStorage.getItem("listDonHang") ? JSON.parse(localStorage.getItem("listDonHang")) : [];
+    
+        // kiểm tra trùng thông tin đăng ký
+        var tonTai = false;
+        if(listTaiKhoan != []) {
+            for (var i = 0; i < listTaiKhoan.length;i++) {
+                if(listTaiKhoan[i].hoten === hoten.value ) {
+                    tonTai = true;
+                    hoten.parentElement.querySelector(".error-message").innerText = "Họ tên này đã tồn tại";
+                    hoten.parentElement.querySelector(".error-message").style.color = "red";
+                    hoten.focus();
+                    return false;
+    
+                } else if (listTaiKhoan[i].email === email.value) {
+                    tonTai = true;
+                    email.parentElement.querySelector(".error-message").innerText = "Email này đã tồn tại";
+                    email.parentElement.querySelector(".error-message").style.color = "red";
+                    email.focus();
+                    return false;
+                } else if (listTaiKhoan[i].sdt === sdt.value) {
+                    tonTai = true;
+                    sdt.parentElement.querySelector(".error-message").innerText = "Số điện thoại này đã tồn tại";
+                    sdt.parentElement.querySelector(".error-message").style.color = "red";
+                    sdt.focus();
+                    return false;
+                } else if (listTaiKhoan[i].matkhau === matkhau.value) {
+                    tonTai = true;
+                    matkhau.parentElement.parentElement.querySelector(".error-message").innerText = "Mật khẩu này đã tồn tại";
+                    matkhau.parentElement.parentElement.querySelector(".error-message").style.color = "red";
+                    matkhau.focus();
+                    return false;
+                }
+                else {
+                    hoten.parentElement.querySelector(".error-message").innerText = "";
+                    sdt.parentElement.querySelector(".error-message").innerText = "";
+                    email.parentElement.querySelector(".error-message").innerText = "";
+                    matkhau.parentElement.parentElement.querySelector(".error-message").innerText = "";
+                    nhaplaimk.parentElement.parentElement.querySelector(".error-message").innerText = "";
+                }
+            }
+        }
+        // nếu không bị trùng thông tin đăng ký
+        if (tonTai == false) {
+            listTaiKhoan.push({
+            hoten: hoten.value,
+            sdt: sdt.value,
+            email: email.value,
+            matkhau: matkhau.value,
+        })
+        localStorage.setItem("listTaiKhoan", JSON.stringify(listTaiKhoan));
+        alert("Đăng Ký thành công!");
+    
+        // khi tạo tài khoản ==> tạo luôn kho lưu trữ giỏ hàng cho khách
+        listGioHang.push({
+            name: hoten.value,
+            sdt: sdt.value,
+            email: email.value,
+            matkhau: matkhau.value,
+            giohang: [],
+        })
+        localStorage.setItem("listGioHang", JSON.stringify(listGioHang));
+    
+    
+        listDonHang.push({
+            name: hoten.value,
+            sdt: sdt.value,
+            email: email.value,
+            matkhau: matkhau.value,
+            donhang: [],
+        })
+        localStorage.setItem("listDonHang", JSON.stringify(listDonHang));
+        }
+        dongDangKy();
+    }
+    
+    function dangNhap() {
+        var taikhoan = document.getElementById("taiKhoan").value;
+        var matkhau = document.getElementById("password_3").value;
+        var listTaiKhoan = localStorage.getItem("listTaiKhoan") ? JSON.parse(localStorage.getItem("listTaiKhoan")) : [];
+        var check = false;
+        for (let index = 0; index < listTaiKhoan.length; index++) {
+            if ((listTaiKhoan[index].sdt === taikhoan || listTaiKhoan[index].email === taikhoan) && listTaiKhoan[index].matkhau === matkhau) {
+                check = true;
+                break;
+            }
+        }
+
+        // Kiểm tra tài khoản đã tồn tại hay chưa
+    
+        // tài khoản đã tồn tại
+        if (check) {
+            document.getElementById("password_3").parentElement.parentElement.querySelector(".error-message").innerText = "";
+            alert("Đăng nhập thành công");
+            dongDangNhap();
+    
+            var listTaiKhoan = localStorage.getItem("listTaiKhoan") ? JSON.parse(localStorage.getItem("listTaiKhoan")) : [];
+            var hoten,sdt,email;
+            for (const tk of listTaiKhoan) {
+                if (tk.matkhau === matkhau) {
+                        hoten = tk.hoten;
+                        sdt = tk.sdt;
+                        email = tk.email;
+                }
+            }
+            // khi đã đăng nhập vào thì thay Đăng nhập ==> Đăng xuất
+            document.getElementById("Dang_nhap").outerHTML = `
+            <div id="Dang_xuat" onclick="dangXuat();">
+            <i class="fas fa-sign-in-alt"></i>
+            <a href="#">Đăng Xuất</a>
+            </div>`;
+    
+            // In thông tin khách hàng
+            document.getElementById("Dang_ky").style.display = 'none';
+            document.getElementById("user").style.display = "block";
+            document.getElementById("user").innerHTML = `
+                <i class="fas fa-user"></i>
+                <a href="#">${hoten}</a>
+                <div id="infomation">
+                    <p>Họ tên: <span id="show-hoten">${hoten}</span></p>
+                    <p>Số điện thoại: <span id="show-sdt">${sdt}</span></p>
+                    <p>Email: <span id="show-taikhoan">${email}</span></p>
+                    <p>Mật Khẩu: <span id="show-matkhau">${matkhau}</span></p>
+                </div>`;
+    
+            // show Đơn hàng đã đặt
+            document.getElementById("order").style.display = "block";
+            // cập nhật giỏ hàng cho khách hàng (lưu lại giỏ hàng của khách khi thoát)  
+            var listGioHang = JSON.parse(localStorage.getItem("listGioHang"));
+            var stt = 0;
+            var s = "";
+            var tongtien = 0;
+            for (const a of listGioHang) {
+                if (a.matkhau === matkhau) {
+                    for (const b of a.giohang) {
+                        s += `            
+                        <tr class="sanPham">
+                            <td class="stt">${++stt}</td>
+                            <td><img class="hinh" src=${b.img}></td>
+                            <td class="cotTen">${b.nameProduct}</td>
+                            <td class="donGia">${b.price}đ</td>
+                            <td class="soLuong">${b.quantity}</td>
+                            <td class="thanhTien">${b.money}đ</td>
+                            <td>
+                                <button onclick="deleteProduct(this);">Delete</button>
+                            </td>
+                        </tr>`;
+                        tongtien += parseInt(b.money);
+                    }
+                    document.getElementById("showShopTable").innerHTML = s;
+                }
+            }
+            console.log(stt);
+            document.getElementById("quantity").innerText = stt;
+            document.getElementById("tongTien").innerText = `${tongtien}đ`;
+        }
+        // tài khoản chưa tồn tại
+        else {
+            document.getElementById("password_3").parentElement.parentElement.querySelector(".error-message").innerText = "Tài khoản không tồn tại. Vui lòng đăng ký!";
+            document.getElementById("password_3").parentElement.parentElement.querySelector(".error-message").style.color = "red";
+        }
+    
+        // tài khoản đặc biệt (admin) ==> dẫn tới trang admin
+        if (taikhoan === adminTk && matkhau === adminMk) {
+            window.location.href = "http://127.0.0.1:5500/admin.html";
+        }
+    }
+    
+    
+    var click = false;
+    function showInfo() {
+        if (!click) {
+            document.getElementById("infomation").style.display = "block";
+            click = true;
+        }
+        else {
+            document.getElementById("infomation").style.display = "none";
+            click = false;
+        }
+    }
+
+    click = false;
+    function showOrder() {
+    if (!click) {
+        document.getElementById("main-order").style.display = "block";
+        click = true;
+        inDonHang();
+    }
+    else {
+        document.getElementById("main-order").style.display = "none";
+        click = false;
+    }
+}
+    
+    
+    function dangXuat() {
+        document.getElementById("user").innerHTML = "";
+        document.getElementById("user").style.display = "none";
+        document.getElementById("order").style.display = "none";
+        document.getElementById("Dang_ky").style.display = 'block';
+        document.getElementById("Dang_xuat").outerHTML = `
+        <div id="Dang_nhap" onclick="moDangNhap()">
+        <i class="fas fa-sign-in-alt"></i>
+        <a href="#">Đăng nhập</a>
+        </div>`
+    
+        // document.getElementById("quantity").innerText = 0;
+        // document.getElementById("tongTien").innerHTML = `0đ`;
+        // var display = document.getElementById("showShop").style.display;
+        // if (display === "block") {
+        //     document.getElementById("showShop").style.display = "none";
+        //     console.log(document.getElementById("showShop").style.display);
+        // }
+        // document.getElementById("order").style.display = "none";
+    
+    }
+    
+    
+
+
+
+
+
+
+
+
+
+
+
+
+    
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 // 		Code slider			
 const right = document.querySelector(".next");
@@ -48,63 +422,6 @@ left.addEventListener("click", () => {
 
 
 
-({
-    plugin: ['jsdom-quokka-plugin'],
-    jsdom: 
-    {
-        file: "do_an.html"
-    }
-})
-//bỏ cái ở trên
-
-
-
-
-let slash = document.getElementsByClassName("slash");
-let password_input = document.getElementsByClassName("password");
-
-const see_hide = document.getElementsByClassName("place");
-const exits = document.getElementsByClassName("exit");
-
-
-
-let form_1 = document.getElementById("sign_up").style.zIndex, form_2 = document.getElementById("sign_in").style.zIndex; 
-
-document.getElementById("Dang_ky").onclick = () => document.getElementById("sign_up").style.zIndex = 1;
-document.getElementById("Dang_nhap").onclick = () => document.getElementById("sign_in").style.zIndex = 1;
-
-
-// console.log( document.getElementById("sign_up"))
-for (let exit of exits)
-    exit.onclick = () =>
-    {
-        document.getElementById("sign_in").style.zIndex = -1;
-        document.getElementById("sign_up").style.zIndex = -1;       
-        for (let i = 0; i < password_input.length; i++)
-            if ( password_input[i].type === "text")
-            {
-                password_input[i].type = "password";
-                slash[i].style.zIndex = 1;
-            }
-    }
-
-for (let i = 0; i < see_hide.length; i++)
-    see_hide[i].onclick = () =>
-    {
-        if ( password_input[i].type === "password")
-        {
-            password_input[i].type = "text";
-            slash[i].style.zIndex = -1;
-        }
-        else
-        {
-            password_input[i].type = "password";
-            slash[i].style.zIndex = 1;
-        }
-        password_input[i].focus();
-    }
-
-	
 			//		 Hiện màu tab + Chuyển tab		//
 		const $ = document.querySelector.bind(document);
 		const $$ = document.querySelectorAll.bind(document);
